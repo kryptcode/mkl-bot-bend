810666017ba24e5d478578173281bd4f579d0c9d
Koye <54333700+kryptcode@users.noreply.github.com>
2024-06-14 11:13:02 +0100

    "cors"

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { execFile } = require('child_process');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
 const port = 8000;
+
+app.use(cors())
 
 app.use(bodyParser.json());
 
@@ -13,8 +16,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/run-notebook', (req, res) => {
-  const userInput = req.body.userInput; // Get user input from the request body
-  const notebookPath = path.join(__dirname, 'mkl.ipynb');
+  const userInput = req.body.userInput;
+  const notebookPath = path.join(__dirname, 'chatbot.ipynb');
 
   execFile('python', ['execute_notebook.py', notebookPath, userInput], (error, stdout, stderr) => {
     if (error) {
